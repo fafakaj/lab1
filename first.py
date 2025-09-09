db9411cedccb4746513403ca71f9105e1f0313f9
fafakaj <mrainxxxm@gmail.com>
2025-09-09 12:22:33 +0300

    Add files via upload

--- first.py
+++ first.py
@@ -1,15 +1,34 @@
 import unittest
 
 def simplesum(nums, target):
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target and nums[i] != nums[j]:
-                return[i, j]
-    return "NO"
+
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if type(nums[i]) == str or type(nums[j]) == str:
+                    continue
+                elif nums[i] + nums[j] == target:
+                    return[i, j]
+        return "NO"
 
 class TestMySum(unittest.TestCase):
     def test_1(self):
         self.assertEqual(simplesum([1,2,3,4,5], 6), [0,4])
+    def test_2(self):
+        self.assertEqual(simplesum([2,7,11,15], 9), [0,1])
+    def test_3(self):
+        self.assertEqual(simplesum([3,2,4], 6), [1,2])
+    def test_4(self):
+        self.assertEqual(simplesum([3,3], 6), [0,1])
+    def test_5(self):
+        self.assertEqual(simplesum([3,2,"dsds"], 5), [0,1])
+    def test_6(self):
+        self.assertEqual(simplesum(["sasasa",4,8,2], 10),[2,3])
+    def test_7(self):
+        self.assertEqual(simplesum([1,2,3,4], 100),"NO")
+
+
+
+
 
 if __name__ == '__main__':
-    unittest.main(argv=[''], exit=False)  # для интерактивных сред
+    unittest.main()
